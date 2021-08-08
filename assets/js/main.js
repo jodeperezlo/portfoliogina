049574720b4b25318d3b9c07eaab94517560b326
Jorge de J. Pérez López <jodeperezlo@ittepic.edu.mx>
2021-08-08 01:00:53 -0600

    Validate Form
    Se validó el formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,28 @@ portfolio.addEventListener('click', e => {
     }
 });
 
+// ------------------------------------------------------------
+// Validate form
+// ------------------------------------------------------------
+(function () {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
 /*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
