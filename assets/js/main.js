98e9982aa6e6f929e34ab9a871fa3466a6b40995
Jorge de J. Pérez López <jodeperezlo@ittepic.edu.mx>
2021-08-07 22:42:09 -0600

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 200;
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){            
@@ -49,3 +49,11 @@ portfolio.addEventListener('click', e => {
         modalImgPortfolio.src = urlImg;
     }
 });
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-up class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
